4c392449004c292738e3fed835d57674cee2f40f
jessica63256325-dot <jessica63256325@gmail.com>
2026-07-23 18:47:04 +0600

    Update main.py
    import asyncio
    from telegram import Bot
    
    BOT_TOKEN = "8712541872:AAG9qHuLKGTSDkmK9AIcbXGfZU4Y8W5KxDg"
    GROUP_ID = -1004347995330
    
    MESSAGE = """
    🔥 NL BL EXPOSED 🔥
    
    ✅ Join Now
    https://is.gd/3MfYhX
    
    Select age 26+ on my page ❤️
    
    #Netherlands #Belgium
    """
    
    async def main():
    bot = Bot(token=BOT_TOKEN)
    await bot.send_message(chat_id=GROUP_ID, text=MESSAGE)
    
    asyncio.run(main())

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("NLBLAutoShareBot is running!")
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = "8712541872:AAG9qHuLKGTSDkmK9AIcbXGfZU4Y8W5KxDg"
+GROUP_ID = -1004347995330
+
+MESSAGE = """
+🔥 NL BL EXPOSED 🔥
+
+✅ Join Now
+https://is.gd/3MfYhX
+
+Select age 26+ on my page ❤️
+
+#Netherlands #Belgium
+"""
+
+async def main():
+bot = Bot(token=BOT_TOKEN)
+await bot.send_message(chat_id=GROUP_ID, text=MESSAGE)
+
+asyncio.run(main())
